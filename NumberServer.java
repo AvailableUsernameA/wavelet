058d450996d20e79fc1c4608162f7b85ce705b39
AvailableUsernameA <huc014@ucsd.edu>
2022-10-06 11:36:48 -0700

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -8,20 +8,22 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Number: %d", num);  //If the path of url is /, then num doesn't change
         } else if (url.getPath().equals("/increment")) {
-            num += 1;
+            num += 1;   //If the path of url is /increment, then 1 is added to num
             return String.format("Number incremented!");
         } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            System.out.println("Path: " + url.getPath());   //If the path is neither / nor /increment, then print out the path
+            if (url.getPath().contains("/add")) {   //is this path contains /add
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("count")) {  //then is the query of the url has count at the front
+                    num += Integer.parseInt(parameters[1]);   //then add the number after count= to num
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);   
+                    //return the number added and the total numcer
                 }
             }
             return "404 Not Found!";
+            //If the path is not / or /increment and deson't contain /add, then return 404 not found.
         }
     }
 }
@@ -30,11 +32,12 @@ class NumberServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
+            //Check that the input can't be empty
             return;
         }
 
-        int port = Integer.parseInt(args[0]);
+        int port = Integer.parseInt(args[0]);  //Get the port from the first input and turns it into integer
 
-        Server.start(port, new Handler());
+        Server.start(port, new Handler());   //It calls Server.start method.
     }
 }
